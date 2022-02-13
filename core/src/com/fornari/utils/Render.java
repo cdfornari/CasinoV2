49516f18859ad64e935adf21e9abc44a5268fce8
Carlos Fornari <lilfornah@gmail.com>
2022-02-13 01:29:38 -0400

    Guia de instalacion

--- core/src/com/fornari/utils/Render.java
+++ core/src/com/fornari/utils/Render.java
@@ -1,12 +1,10 @@
 package com.fornari.utils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.fornari.casino.Casino;
-import com.fornari.screens.GameScreen;
 
 public class Render {
 	public static SpriteBatch batch;
